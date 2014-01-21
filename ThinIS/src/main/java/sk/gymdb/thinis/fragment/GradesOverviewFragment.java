aae550b0454de079c8811f48193dfb9b5c66b818
adamlaurencik <laurencik.a@gmail.com>
2014-01-21 22:53:26 +0100

    grades design

--- ThinIS/src/main/java/sk/gymdb/thinis/fragment/GradesOverviewFragment.java
+++ ThinIS/src/main/java/sk/gymdb/thinis/fragment/GradesOverviewFragment.java
@@ -32,27 +32,51 @@ public class GradesOverviewFragment extends Fragment {
 
         // todo tuto zobraz znamky, ale len tie, ktore mas uz ulozene v preferences, nenacitavaj ich pokazde
         // todo nacitavanie sa ma spravit pri logine a potom pri stlaceni tlacidla refresh v hornom actionbare
-        Context context= getActivity().getApplicationContext();
+        Context context = getActivity().getApplicationContext();
         // gets grades from SharedPreferences and makes Userinfo from Json
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-        Gson gson=new Gson();
-        String name = prefs.getString("name", "");
+        Gson gson = new Gson();
         String gradesJson = prefs.getString("grades", "");
         LinkedHashMap<String, ArrayList<String>> info = new Gson().fromJson(gradesJson, LinkedHashMap.class);
-        TableLayout table= (TableLayout) rootView.findViewById(R.id.table);
-        TextView textView= (TextView) rootView.findViewById(R.id.GradesText);
-        textView.setText(name);
+        TableLayout table = (TableLayout) rootView.findViewById(R.id.table);
+        table.setShrinkAllColumns(true);
+        TableRow header = new TableRow(context);
+        TextView headerClazz = new TextView(context);
+        TextView headerGrades = new TextView(context);
+        headerClazz.setText("Predmet");
+        headerGrades.setText("Známky");
+        headerClazz.setTextSize(20);
+        headerGrades.setTextSize(20);
+        header.addView(headerClazz);
+        header.addView(headerGrades);
+        table.addView(header);
+        // loops trough keys of HashMap, keys are subjcet names
         for (String key : info.keySet()) {
+            // creates new table row and two textViews
             TableRow row = new TableRow(context);
-            TextView subject= new TextView(context);
-            TextView grades= new TextView(context);
+            TextView subject = new TextView(context);
+            TextView grades = new TextView(context);
+            grades.setPadding(2, 2, 2, 0);
+            subject.setPadding(2, 2, 2, 0);
+            // makes TextViews adapt to the text (if it is longer than one row it puts text in it into another row)
+            subject.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT));
+            grades.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT));
             subject.setText(key);
-            grades.setText(info.get(key).toString());
+            String gradesText = "";
+            // loops trough ArrayList of grades and writes them to string, simple toString() will show it with []
+            for (String s : info.get(key)) {
+                if (gradesText.equals("")) {
+                    gradesText = s + ", ";
+                } else {
+                    gradesText = gradesText + s + ", ";
+                }
+                ;
+            }
+            grades.setText(gradesText);
             row.addView(subject);
             row.addView(grades);
             table.addView(row);
         }
-
 
 
         return table;
